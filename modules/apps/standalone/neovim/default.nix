913f63ed766df68d5f1ab6086a5ab01180831ca9
arkaitzsilva <arkaitz.develop@gmail.com>
2025-07-02 19:28:29 +0200

    basic initial nvf config

--- modules/apps/standalone/neovim/default.nix
+++ modules/apps/standalone/neovim/default.nix
@@ -3,36 +3,39 @@
   pkgs,
   lib,
   defaults,
+  inputs,
   ...
 }:
 with lib;
 with lib.shelf; let
   cfg = config.shelf.apps.standalone.neovim;
-in {
+in with lib; {
+  imports = [ inputs.nvf.nixosModules.default ];
+
   options.shelf.apps.standalone.neovim = {
     enable = mkBoolOpt false "Whether to enable neovim editor.";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-
-    ];
+    environment.systemPackages = with pkgs; [];
 
     programs.neovim = {
       enable = true;
       defaultEditor = true;
     };
 
-    # shelf.home.programs.nvf = {
-    #   enable = true;
-    #   vim = {
-    #     theme = {
-    #       enable = true;
-    #       name = "nord";
-    #       style = "dark";
-    #     };
-    #   };
-    # };
+    programs.nvf = {
+      enable = true;
+      settings = {
+        vim = {
+          theme = {
+            enable = true;
+            name = "${defaults.colorSchemeVariant}";
+            style = "dark";
+          };
+        };
+      };
+    };
     
     #shelf.home.configFile."nvim/init.lua".source = "${defaults.configFolder}/nvim/init.lua";
   };

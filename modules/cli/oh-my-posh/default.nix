9fd6d421d6c4cdfc4f866015ba0a6566e11f9437
arkaitzsilva <arkaitz.develop@gmail.com>
2025-07-30 19:46:39 +0200

    oh-my-posh separate theme

--- modules/cli/oh-my-posh/default.nix
+++ modules/cli/oh-my-posh/default.nix
@@ -9,6 +9,12 @@ with lib;
 with lib.shelf; let
   cfg = config.shelf.cli.oh-my-posh;
   zshEnabled = config.shelf.cli.zsh.enable || config.shelf.system.defaultShell == pkgs.zsh;
+
+  theme = builtins.fromJSON (builtins.readFile "${defaults.configFolder}/oh-my-posh/themes/minimal.json");
+  colorSchemeVariant = builtins.fromJSON (builtins.readFile "${defaults.configFolder}/oh-my-posh/color-scheme-variants/${defaults.colorSchemeVariant}.json");
+  mergedConfig = lib.recursiveUpdate theme colorSchemeVariant;
+
+  jsonFormat = pkgs.formats.json { };
 in {
   options.shelf.cli.oh-my-posh = {
     enable = mkBoolOpt false "Whether to enable oh-my-posh.";
@@ -20,7 +26,7 @@ in {
     shelf.home.programs.oh-my-posh = {
       enable = true;
       enableZshIntegration = zshEnabled;
-      settings = builtins.fromJSON (builtins.readFile "${defaults.configFolder}/oh-my-posh/minimal.json");
+      settings = mergedConfig;
     };
   };
 }

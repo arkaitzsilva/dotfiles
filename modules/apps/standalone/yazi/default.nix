d268175cac6e6ecbd7fd1c124f9829fb22925f86
arkaitzsilva <arkaitz.develop@gmail.com>
2025-08-06 16:01:50 +0200

    yazi & general customization

--- modules/apps/standalone/yazi/default.nix
+++ modules/apps/standalone/yazi/default.nix
@@ -14,13 +14,23 @@ in {
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ ];
+    environment.systemPackages = with pkgs; [];
 
+    services.udisks2.enable = true;
+    
     shelf.home.programs.yazi = {
       enable = true;
+      plugins = with pkgs; {
+        mount = yaziPlugins.mount;
+        git = yaziPlugins.git;
+        full-border = yaziPlugins.full-border;
+      };
     };
 
+
+    shelf.home.configFile."yazi/init.lua".source = "${defaults.configFolder}/yazi/init.lua";
     shelf.home.configFile."yazi/yazi.toml".source = "${defaults.configFolder}/yazi/yazi.toml";
+    shelf.home.configFile."yazi/keymap.toml".source = "${defaults.configFolder}/yazi/keymap.toml";
     shelf.home.configFile."yazi/theme.toml".source = "${defaults.configFolder}/yazi/color-scheme-variants/${defaults.colorSchemeVariant}.toml";
   };
 }

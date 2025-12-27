cdf6b26b11daea60e82953719cce177ac61aeefc
arkaitzsilva <arkaitz.develop@gmail.com>
2025-12-27 11:01:11 +0100

    change miror awww settings

--- modules/desktop/addons/flatpak/default.nix
+++ modules/desktop/addons/flatpak/default.nix
@@ -40,7 +40,7 @@ in {
   config = mkIf cfg.enable {
     # Flatpak
     shelf.home.sessionVariables = {
-      XDG_DATA_DIRS="$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
+      XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
     };
 
     services.flatpak.enable = true;

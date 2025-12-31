7ad734bca2dbbf2c8ceead8182ab7746eb1d5f10
arkaitzsilva <arkaitz.develop@gmail.com>
2025-12-31 19:27:04 +0100

    Fix GTK4 styles, use selected theme.

--- modules/desktop/addons/gtk/default.nix
+++ modules/desktop/addons/gtk/default.nix
@@ -78,8 +78,13 @@ in {
         gtk-application-prefer-dark-theme = 1;
       };
 
-      gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
+      gtk4 = {
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
+        extraCss = ''
+          @import url("file://${gtkThemePkg}/share/themes/${gtkColorSchemeVariant}/gtk-4.0/gtk.css");
+        '';
       };
     };
   };
